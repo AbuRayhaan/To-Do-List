d4701d6777c62011f41ff40b542913bf14f83070
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-04-27 10:31:40 +0100

    Modified the src/index.js file to impelement object/DOM/Function manipulation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import './style.css';
+
+const toDoTask = [
+  {
+    index: 1,
+    description: 'Wash the dishes',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'Complete To Do list project',
+    completed: true,
+  },
+  {
+    index: 3,
+    description: 'Lunch',
+    completed: true,
+  },
+  {
+    index: 4,
+    description: 'Stand up meeting',
+    completed: true,
+  },
+];
+
+const toDoList = document.querySelector('#task-container');
+
+const listDisplay = () => {
+  let display = '';
+  toDoTask.forEach((task) => {
+    display += `<li class="task-item-container" id=${task.index}>
+    <div class="task-item">
+      <input type="checkbox" id=${task.index} ${task.completed ? 'checked' : ''}>
+      <label type="text" id="${task.index}" for="${task.description}">${task.description}</label>
+    </div>
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+    </li>
+    `;
+  });
+  toDoList.innerHTML = display;
+};
+
+listDisplay();
