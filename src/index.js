0af1f6f49c7c6de7d75663831c2cbfb5a093dcc6
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-04-28 10:32:34 +0100

    Modified the index.js file

--- src/index.js
+++ src/index.js
@@ -1,55 +1,96 @@
 import './style.css';
-/*
-const toDoTask = [
-  {
-    index: 1,
-    description: 'Wash the dishes',
-    completed: true,
-  },
-  {
-    index: 2,
-    description: 'Complete To Do list project',
-    completed: true,
-  },
-  {
-    index: 3,
-    description: 'Lunch',
-    completed: true,
-  },
-  {
-    index: 4,
-    description: 'Stand up meeting',
-    completed: true,
-  },
-];
-*/
+import updateList from './listUpdate.js';
+
 const toDoList = document.querySelector('#task-container');
 const addTask = document.getElementById('#addTask');
 const addTaskInput = document.getElementById('#addTaskInput');
 let storedTasks = [];
+let editTaskItem = null;
 
 const getTask = () => {
-  if(localStorage.getItem('tasks') === null) {
+  if (localStorage.getItem('tasks') === null) {
     storedTasks = [];
   } else {
     storedTasks = JSON.parse(localStorage.getItem('tasks'));
   }
 
-}
-
-const listDisplay = () => {
   let display = '';
-  toDoTask.forEach((task) => {
-    display += `<li class="task-item-container" id=${task.index}>
-    <div class="task-item">
-      <input type="checkbox" id=${task.index} ${task.completed ? 'checked' : ''}>
-      <label type="text" id="${task.index}" for="${task.description}">${task.description}</label>
-    </div>
-    <i class="fa-solid fa-ellipsis-vertical"></i>
-    </li>
-    `;
+  storedTasks.forEach((task, index) => {
+    const completed = task.completed ? 'line-through' : '';
+    display += `<li class="task-item-container ${completed}" id=${task}>
+      <div class="task-item">
+        <input type="checkbox" id=${index} ${task.completed ? 'checked' : ''}>
+        <label type="text" id="${index}" for="${task.description}">${task.description}</label>
+      </div>
+      <i class="fa-solid fa-trash-can" id="${index}></i>
+      <i class="fa-solid fa-ellipsis-vertical" id="${index}></i>
+      </li>
+      `;
   });
+
   toDoList.innerHTML = display;
 };
 
-listDisplay();
+const saveTask = ({ index, description, completed = false }) => {
+  storedTasks = [];
+
+  if (localStorage.getItem('tasks') === null) {
+    storedTasks = [];
+  } else {
+    storedTasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  storedTasks.push({ index, description, completed });
+  localStorage.setItem('tasks', JSON.stringify(storedTasks));
+  getTask();
+};
+
+const saveEditTask = (task) => {
+  const taskItem = storedTasks[task];
+  taskItem.description = addTaskInput.value;
+  localStorage.setItem('tasks', JSON.stringify(storedTasks));
+};
+
+const resetList = (storedList) => storedList.forEach((taskItem, index) => {
+  taskItem.index = index;
+});
+
+const deleteTask = (task) => {
+  const delItem = storedTasks[task];
+  storedTasks = storedTasks.filter((toDel) => toDel !== delItem);
+  resetList(storedTasks);
+  localStorage.setItem('tasks', JSON.stringify(storedTasks));
+};
+
+const editTask = (task) => {
+  editTaskItem = task;
+  const taskEdit = storedTasks[task];
+  addTaskInput.value = taskEdit.description;
+  addTaskInput.focus();
+};
+
+addTask.addEventListener('click', (addItem) => {
+  addItem.preventDefault();
+  if (!addTaskInput.value) return;
+
+  if (editTaskItem != null) {
+    saveEditTask(editTaskItem);
+    editTaskItem = null;
+  } else {
+    saveTask({ index: storedTasks.length, description: addTaskInput.value, completed: false})
+  }
+
+  getTask();
+  addTaskInput.value = '';
+});
+
+document.addEventListener('keydown', (press) => {
+  if (press.key === 'Enter' && addTaskInput.value) {
+    addTask.click();
+  }
+});
+
+document.addEventListener('DOMContentLoaded', getTask);
+toDoList.addEventListener('click', updateList);
+
+export { deleteTask, editTask };
