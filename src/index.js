17cfc444b9277f2ac1494d42c81692fdb3e9fe69
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-04-28 19:42:01 +0100

    Modified the index.js file to adjust task's display

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './style.css';
-import updateList from './listUpdate.js';
 
 const toDoList = document.querySelector('#task-container');
-const addTask = document.getElementById('#addTask');
-const addTaskInput = document.getElementById('#addTaskInput');
+const addTask = document.getElementById('addTask');
+const addTaskInput = document.getElementById('addTaskInput');
 let storedTasks = [];
 let editTaskItem = null;
 
@@ -18,12 +17,22 @@ const getTask = () => {
   storedTasks.forEach((task, index) => {
     const completed = task.completed ? 'line-through' : '';
     display += `<li class="task-item-container ${completed}" id=${task}>
-      <div class="task-item">
-        <input type="checkbox" id=${index} ${task.completed ? 'checked' : ''}>
-        <label type="text" id="${index}" for="${task.description}">${task.description}</label>
-      </div>
-      <i class="fa-solid fa-trash-can" id="${index}></i>
-      <i class="fa-solid fa-ellipsis-vertical" id="${index}></i>
+    <div class = "task-item-container">
+    <div class="task-item">
+      <input type="checkbox" id="${task.index} ${task.completed ? 'checked' : ''}">
+      <label type="text" id="">${task.description}</label>
+    </div>
+    <i class="fa-solid fa-ellipsis-vertical" id ="ellipse"></i>
+    </div>
+    </li>
+
+    <div class = "task-item-container-2">
+    <div class="task-item">
+      <input type="checkbox" id="${task.index} ${task.completed ? 'checked' : ''}">
+      <label type="text" id="editTask">${task.description}</label>
+    </div>
+    <i class="fa-solid fa-trash-can" id ="delete"></i>
+    </div>
       </li>
       `;
   });
@@ -50,11 +59,28 @@ const saveEditTask = (task) => {
   taskItem.description = addTaskInput.value;
   localStorage.setItem('tasks', JSON.stringify(storedTasks));
 };
+/*
+const completedTask = (task) => {
+  const input = task.querySelector('input');
+  const label = task.querySelector('label');
+  const taskItem = storedTasks[task.id];
+
+  if (input.checked) {
+    taskItem.completed = true;
+    label.style.textDecoration = 'line-through';
+  } else {
+    taskItem.completed = false;
+    label.style.textDecoration = 'none';
+  }
+
+  localStorage.setItem('tasks', JSON.stringify(storedTasks));
+  getTask();
+};
 
 const resetList = (storedList) => storedList.forEach((taskItem, index) => {
   taskItem.index = index;
 });
-
+/*
 const deleteTask = (task) => {
   const delItem = storedTasks[task];
   storedTasks = storedTasks.filter((toDel) => toDel !== delItem);
@@ -68,7 +94,7 @@ const editTask = (task) => {
   addTaskInput.value = taskEdit.description;
   addTaskInput.focus();
 };
-
+*/
 addTask.addEventListener('click', (addItem) => {
   addItem.preventDefault();
   if (!addTaskInput.value) return;
@@ -77,7 +103,7 @@ addTask.addEventListener('click', (addItem) => {
     saveEditTask(editTaskItem);
     editTaskItem = null;
   } else {
-    saveTask({ index: storedTasks.length, description: addTaskInput.value, completed: false})
+    saveTask({ index: storedTasks.length, description: addTaskInput.value, completed: false });
   }
 
   getTask();
@@ -89,8 +115,25 @@ document.addEventListener('keydown', (press) => {
     addTask.click();
   }
 });
+/*
+const updateList = (update) => {
+  const selectItem = update.target;
 
-document.addEventListener('DOMContentLoaded', getTask);
+  if (selectItem.classList.contains('checkbox')) {
+    const item = selectItem.closest('li');
+    completedTask(item);
+  }
+
+  if (selectItem.classList.contains('delete')) {
+    const item = selectItem.parentElement.parentElement.id;
+    deleteTask(item);
+  }
+
+  if (selectItem.classList.contains('edit')) {
+    const item = selectItem.parentElement.parentElement.id;
+    editTask(item);
+  }
+};
+*/
+
 toDoList.addEventListener('click', updateList);
-
-export { deleteTask, editTask };
