8bbc3283f76d1b31b34db7348e0d662d5ce89559
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-01 20:35:11 +0100

    Modified the modules directory

---
+++ src/modules/removeTask.js
@@ -0,0 +1,12 @@
+export default function removeTask() {
+  const removeList = document.querySelectorAll('.hide1');
+  const Taskstored = JSON.parse(localStorage.getItem('tasks'));
+  removeList.forEach((a, i) => {
+    document.getElementById(`idBtn${i}`).addEventListener('click', () => {
+      const TaskFiltered = Taskstored.filter((task) => task.index !== i + 1);
+      localStorage.setItem('tasks', JSON.stringify(TaskFiltered));
+      window.location.reload();
+      updateId();
+    });
+  });
+}
